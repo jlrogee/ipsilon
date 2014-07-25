8342f64683fc262966107ea9f1f0ff28ee0f5e89
Vicpo <vicpo@ya.ru>
2014-07-25 17:35:20 +0600

    Fix error spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
+  #extend Enumerize
+  #enumerize :role, in: { user: 1, spec: 2, dispatcher: 3, admin: 4 }, default: :user
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :uploads, :as => :attachable
 
-  extend Enumerize
-  enumerize :role, in: { user: 1, spec: 2, dispatcher: 3, admin: 4 }, default: :user
+
 
 end
