ddf9aa7a2fd916b87cc51e628749dc1318e1138f
Vicpo <vicpo@ya.ru>
2014-08-05 22:32:48 +0600

    fix CRUD#2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
 
     user ||= User.new
-    can [:kb, :instr, :knowledge_base], KnowledgeBase
+    can [:kb, :instr], KnowledgeBase
     if user.role.admin? || user.role.dispatcher?
       can :manage, :all
     elsif user.role.user? || user.role.spec?
