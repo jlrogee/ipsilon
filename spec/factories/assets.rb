5a951aa8a3cab13909ec72668b2f625051c03e92
AlexandrUng <a.ungemah@gmail.com>
2014-08-07 14:49:24 +0600

    validatons assets, categories, uploads, tests 2

--- spec/factories/assets.rb
+++ spec/factories/assets.rb
@@ -4,8 +4,7 @@ FactoryGirl.define do
   factory :asset do
     name { Faker::Name.title }
     account_number { Faker::Lorem.word }
-    organization_id { |c| c.association(:organization)} #{ '1' } 
-    user_id { |c| c.association(:user)} #{ '1' } 
+    association :organization, :user
 
   end
 end
