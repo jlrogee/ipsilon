e5fd6e09faca289993c68e72d656faa749132a6c
jlrogee <airts@bk.ru>
2014-08-11 16:49:46 +0600

    user controller fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,10 @@ class UsersController < ApplicationController
   load_and_authorize_resource param_method: :user_params
   before_action :set_user, only: [:show, :edit, :update]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     #redirect_to users_path if @user != current_user
   end
@@ -25,23 +21,16 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email, :firstname, :lastname, :phone, :role,:departament)
     end
