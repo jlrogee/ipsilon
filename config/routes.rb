779b126a2868c9061426586ce521badc10d4820a
jlrogee <airts@bk.ru>
2014-08-11 16:44:08 +0600

    user admin fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations'}
-  resources :users do
-    resources :assets
-  end
-
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
